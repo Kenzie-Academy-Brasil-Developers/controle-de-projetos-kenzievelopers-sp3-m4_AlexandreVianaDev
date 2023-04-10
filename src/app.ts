f38d50629bdec31efb31d74f8ff18768d2ee05d3
AlexandreVianaDev <alexandreviana94@hotmail.com>
2023-04-10 19:23:14 -0300

    feat: routes base

--- src/app.ts
+++ src/app.ts
@@ -3,4 +3,9 @@ import "dotenv/config";
 
 const app: Application = express();
 
+app.post("/developers");
+app.patch("/developers/:id");
+app.delete("/developers/:id");
+app.post("/developers/:id/infos");
+
 export default app;
